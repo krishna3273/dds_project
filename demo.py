d44578df815770a5c7abd32bd287797786f3a6b8
krishna3273 <krishnamaheshteja.n404@gmail.com>
2021-01-18 00:18:05 +0530

    Code for establishing connection

--- demo.py
+++ demo.py
@@ -1,5 +1,4 @@
-from mysql.connector import  connection
-import mysql.connector
+import pymysql
 from sshtunnel import SSHTunnelForwarder
 
 HOST = "10.3.5.215"
@@ -7,20 +6,43 @@ DATABASE_NAME = "Zoo"
 USER = "krishna"
 PASSWORD = "iiit123"
 PORT = 22
-
 SSH_UNAME = "user"
 SSH_PASS = "iiit123"
 DB_HOST = "127.0.0.1"
 
-tunnel = SSHTunnelForwarder(('10.3.5.215', PORT), ssh_password=SSH_PASS, ssh_username=SSH_UNAME,
-     remote_bind_address=(DB_HOST, 3306))
-tunnel.start()
-conn = mysql.connector.connect(host='127.0.0.1', user=USER, passwd=PASSWORD, port=tunnel.local_bind_port)
-cur = conn.cursor(buffered=True)
-cur.execute("SHOW DATABASES;")
-row = cur.fetchone()
-while row is not None:
-    print(row)
-    row = cur.fetchone()
-cur.close()
-conn.close()
+class DB():
+    def _init_(self):
+        self.cursor = None
+        self.conn = None
+        pass
+
+    def connect(self):
+        tunnel = SSHTunnelForwarder((HOST, PORT), ssh_password=SSH_PASS, ssh_username=SSH_UNAME,
+             remote_bind_address=(DB_HOST, 3306))
+        tunnel.start()
+        conn = pymysql.connect(host='127.0.0.1', user=USER, passwd=PASSWORD, port=tunnel.local_bind_port)
+        self.cursor = conn.cursor()
+        self.conn = conn
+
+    def exec(self, query):
+        self.cursor.execute(query)
+        row = self.cursor.fetchone()
+        while row is not None:
+            print(row)
+            row = self.cursor.fetchone()
+
+    def close_cursor(self):
+        self.cursor.close()
+
+    def close_connection(self):
+        try:
+            self.close_cursor()
+            self.conn.close()
+        except:
+            pass
+
+db = DB()
+db.connect()
+db.exec("SHOW DATABASES;")
+db.exec("USE  Zoo;")
+db.close_connection()
