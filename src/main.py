0fcf29fe9599a4886d879f6ad9b6c5d719d6762e
jashwanth10 <jashwanth.gajjala@gmail.com>
2021-01-25 22:34:13 +0530

    Catalog schema

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from connection import DB
+from database import DB
 config = {
-    "HOST": "10.3.5.215",
+    "HOST": "10.3.5.211",
     "DATABASE_NAME": "Zoo",
     "USER": "krishna",
     "PASSWORD": "iiit123",
@@ -10,11 +10,20 @@ config = {
     "DB_HOST": "127.0.0.1",
 }
 
+def init_relations(db):
+    db.exec("CREATE TABLE CONFIG (relation_name varchar(255), fragmentation_type int)")
+    db.exec("CREATE TABLE HF (fragment_id int, relation varchar (255), attribute varchar(255), op varchar(255), value varchar(255), datatype varchar(255), site_id int, PRIMARY KEY (fragment_id))")
+    db.exec("CREATE TABLE SITEINFO (site_id int, ip_address varchar(255), user varchar(255), password varchar(255), PRIMARY KEY (site_id))")
+    db.exec("CREATE TABLE VF (fragment_id int, relation varchar(255), attribute varchar(255), site_id int)")
+    db.exec("CREATE TABLE METADATA (relation varchar(255), derived_relation varchar(255))")
+    db.exec("CREATE TABLE DHF (feagment_id int, relation varchar(255), HF_id int, attribute varchar(255), site_id int)")
 
 def main():
     db = DB(config = config)
     db.connect()
-
-
+    db.exec("USE Zoo;")
+    init_relations(db)
+    db.close_cursor()
+    db.close_connection()
 if __name__ == "__main__" :
     main()
